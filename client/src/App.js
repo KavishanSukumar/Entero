627612f6291ca987ea361e61213d6af607950ddc
shakir saheel <shakirsaheel123@gmail.com>
2022-07-21 20:08:31 +0530

    Created dropdowns for header

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,13 @@
 import Chat from "./components/chat/Chat";
 import { Routes,Route} from 'react-router-dom';
 import Home from "./pages/Home";
+import Events from "./pages/Events";
+import Services from "./pages/Services";
 import ContactUs from "./pages/ContactUs";
 import AboutUs from "./pages/AboutUs";
 import AdminPage from "./pages/AdminPage";
 import AdminEvents from "./pages/AdminEvents";
+
 
 
 function App() {
@@ -15,6 +18,8 @@ function App() {
       <Routes>
                    
         <Route exact path="/" element={<Home/>} />
+        <Route exact path="/events" element={<Events/>} />
+        <Route exact path="/services" element={<Services/>} />
         <Route exact path="/aboutus" element={<AboutUs/>} />
         <Route exact path="/contactus" element={<ContactUs/>} />
         <Route exact path="/adminpage" element={<AdminPage />} />
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -7,10 +7,51 @@ function Header() {
   
         const [nav, setNav] = useState(false);
         
-
         const handleNav = () => {
         setNav(!nav);
         };
+
+        const[dropEvent,setDropEvent]=useState(false);
+
+        const handleDropEvent=()=>{
+          setDropEvent(!dropEvent)
+        }
+
+        const[dropService,setDropService]=useState(false);
+
+        const handleDropService=()=>{
+          setDropService(!dropService)
+        }
+
+        const dropdownEvent=(
+          <div className="hidden md:block absolute top-12 md:left-[24%] lg:left-[36%] w-44 bg-white rounded divide-y divide-gray-100 shadow ">
+            <ul class="py-1 text-sm text-gray-700" >
+              <li className="block py-2 px-4 hover:bg-gray-100">
+              Event 1
+              </li>
+              <li className="block py-2 px-4 hover:bg-gray-100">
+              Event 2
+              </li>
+            </ul>
+    
+          </div>
+      //     
+        );
+
+        const dropdownService=(
+          <div className="hidden md:block absolute top-12 md:left-[36%] lg:left-[44%] w-44 bg-white rounded divide-y divide-gray-100 shadow ">
+            <ul class="py-1 text-sm text-gray-700" >
+              <li className="block py-2 px-4 hover:bg-gray-100">
+              Service 1
+              </li>
+              <li className="block py-2 px-4 hover:bg-gray-100">
+              Service 2
+              </li>
+            </ul>
+    
+          </div>
+      //     
+        );
 
   return (
   
@@ -24,10 +65,16 @@ function Header() {
 
     {/*header links */}
     <ul className='hidden md:flex'>
-      <li className='px-10 py-3 hover:text-white hover:bg-orange-300 hover:rounded'><NavLink className={({isActive})=>isActive? "text-white":""} to="/">Home</NavLink></li>
-      <li className='px-10 py-3 hover:text-white hover:bg-orange-300 hover:rounded'><NavLink className={({isActive})=>isActive? "text-white":""} to="/aboutus">About us</NavLink></li>
-      <li className='px-10 py-3 hover:text-white hover:bg-orange-300 hover:rounded'><NavLink className={({isActive})=>isActive? "text-white":""} to="/contactus">Contact Us</NavLink></li>
+      <li className='px-6 lg:px-8 py-3 hover:text-white hover:bg-orange-300 hover:rounded'><NavLink className={({isActive})=>isActive? "text-white":""} to="/">Home</NavLink>          
+      </li>
+      <li className='px-6 lg:px-8 py-3 hover:text-white hover:bg-orange-300 hover:rounded'><NavLink onMouseEnter={handleDropEvent} className={({isActive})=>isActive? "text-white":""} to="/events">Events</NavLink></li>
+      <li className='px-6 lg:px-8 py-3 hover:text-white hover:bg-orange-300 hover:rounded'><NavLink className={({isActive})=>isActive? "text-white":""} onMouseDown={handleDropService}  to="/services">Services</NavLink></li>
+      <li className='px-6 lg:px-8 py-3 hover:text-white hover:bg-orange-300 hover:rounded'><NavLink className={({isActive})=>isActive? "text-white":""} to="/aboutus">About us</NavLink></li>
+      <li className='px-6 lg:px-8 py-3 hover:text-white hover:bg-orange-300 hover:rounded'><NavLink className={({isActive})=>isActive? "text-white":""} to="/contactus">Contact Us</NavLink></li>
+      
     </ul>
+    {dropEvent? dropdownEvent:""}
+    {dropService? dropdownService:""}
     
     
     {/*login components*/}
@@ -49,11 +96,13 @@ function Header() {
          }
     </div>
 
-    <ul className={nav ? 'fixed left-0 top-9 w-[50%] h-full border-r border-r-gray-900 bg-[#000300]  md:hidden' : ' fixed left-[-100%]'}>
+    <ul className={nav ? 'fixed left-0 top-9 w-[75%] p-8 h-full border-r border-r-gray-900 bg-[#000300]  md:hidden' : ' fixed left-[-100%]'}>
 
         <li className='p-4 border-b border-gray-600 text-white hover:text-blue-500'><NavLink className={({isActive})=>isActive? "text-blue-500":""} to="/">Home</NavLink></li>
+        <li className='p-4 border-b border-gray-600 text-white hover:text-blue-500'><NavLink className={({isActive})=>isActive? "text-blue-500":""} to="/">Events</NavLink></li>
+        <li className='p-4 border-b border-gray-600 text-white hover:text-blue-500'><NavLink className={({isActive})=>isActive? "text-blue-500":""} to="/aboutus">Services</NavLink></li>
         <li className='p-4 border-b border-gray-600 text-white hover:text-blue-500'><NavLink className={({isActive})=>isActive? "text-blue-500":""} to="/aboutus">About us</NavLink></li>
-        <li className='p-4 border-b border-gray-600 text-white hover:text-blue-500'><NavLink className={({isActive})=>isActive? "text-blue-500":""} to="/contactus">Contact Us</NavLink></li>
+        <li className='p-4 border-b mb-2 border-gray-600 text-white hover:text-blue-500'><NavLink className={({isActive})=>isActive? "text-blue-500":""} to="/contactus">Contact Us</NavLink></li>
         <li className='p-4'>
             <Link to="/adminpage" className='bg-blue-500 text-white hover:bg-blue-400 px-5 py-2 rounded'>
             
