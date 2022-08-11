364cfe3b153dec20fe60b179f2c6f319611486de
KavishanSukumar <kavishansukumar@gmail.com>
2022-08-11 22:33:42 +0530

    Portfolio edits

--- client/src/App.js
+++ client/src/App.js
@@ -7,13 +7,12 @@ import PortFolio from "./components/portfolio/PortFolio";
 import Appointment from "./components/appointment/Appointment";
 import ServiceProviderAppointments from "./pages/afterLogin/serviceProvider/ServiceProviderAppointments";
 import BookingsServiceProvider from "./components/booking/BookingsServiceProvider";
-import RegisterService from "./components/register/RegisterService";
-import ChooseUser from "./components/register/ChooseUser";
+
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<ChooseUser />} />
+        <Route path="/" element={<PortFolio />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/customer" element={<CustomerChat />} />
--- client/src/components/portfolio/PortFolio.jsx
+++ client/src/components/portfolio/PortFolio.jsx
@@ -81,7 +81,7 @@ function PortFolio() {
         </div>
       </div>
       <div className="m-3">
-        <Contact />
+        <Packages />
       </div>
     </div>
   );
