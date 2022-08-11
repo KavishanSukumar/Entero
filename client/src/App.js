b016fbd44a2a3d78c29d0b8f4a383d5b37bdf7da
KavishanSukumar <kavishansukumar@gmail.com>
2022-08-11 19:52:51 +0530

    Booking and appointment interfaces

--- client/src/App.js
+++ client/src/App.js
@@ -6,11 +6,14 @@ import CustomerChat from "./pages/afterLogin/customer/CustomerChat";
 import PortFolio from "./components/portfolio/PortFolio";
 import Appointment from "./components/appointment/Appointment";
 import ServiceProviderAppointments from "./pages/afterLogin/serviceProvider/ServiceProviderAppointments";
+import BookingsServiceProvider from "./components/booking/BookingsServiceProvider";
+import RegisterService from "./components/register/RegisterService";
+import ChooseUser from "./components/register/ChooseUser";
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<ServiceProviderAppointments />} />
+        <Route path="/" element={<ChooseUser />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/customer" element={<CustomerChat />} />
