d5973a476957ddb8b4cf97b9a875ca6231e361dd
Senal Punsara <kksenalpunsara@gmail.com>
2022-08-12 16:59:12 +0530

    small edit on managepayment interface

--- client/src/components/managepayment/managepayment.jsx
+++ client/src/components/managepayment/managepayment.jsx
@@ -430,7 +430,7 @@ function ManagePayments() {
             </tr>
             <tr class="bg-white border-b transition duration-300 ease-in-out hover:bg-gray-100">
               <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
-                P001
+                P005
               </td>
 
              
