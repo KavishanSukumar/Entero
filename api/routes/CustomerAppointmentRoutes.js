4d8e11aa85e8ebf52d31a843256b92ca41b5f36e
Yonali Fernando <fernandoymadhushika@gmail.com>
2022-08-21 12:26:34 +0530

    appointment backend done

---
+++ api/routes/CustomerAppointmentRoutes.js
@@ -0,0 +1,54 @@
+const express=require('express');
+const router=express.Router();
+const pool=require("../db");
+
+router.get('/',async (req,res)=>{
+    try{
+        
+        const getCustomerAppointment= await pool.query("SELECT * FROM appointment")
+
+        res.json(getCustomerAppointment.rows);
+    }
+    catch(err){
+        console.log(err.message);
+    }
+})
+
+router.post('/',async (req,res)=>{
+    try{
+        const {date,time,description}=req.body;
+        const newAppointment= await pool.query("INSERT INTO appointment (date,time,description) VALUES ($1,$2,$3) RETURNING *",[date,time,description])
+
+        res.json(newAppointment.rows[0]);
+    }
+    catch(err){
+        console.log(err.message);
+    }
+})
+
+router.put('/:id',async (req,res)=>{
+    try{
+        const {id}=req.params;
+        const {reply}=req.body;
+        const updateAppointment= await pool.query("UPDATE appointment SET date=$1,time=$2,description=$3 WHERE appointment_id=$4",[date,time,description,id])
+
+        res.json("Done updating");
+    }
+    catch(err){
+        console.log(err.message);
+    }
+})
+
+router.delete('/:id',async (req,res)=>{
+    try{
+        const {id}=req.params;
+        const deleteAppointment= await pool.query("DELETE FROM appointment WHERE appointment_id=$1",[id])
+
+        res.json("Done deleting");
+    }
+    catch(err){
+        console.log(err.message);
+    }
+})
+
+module.exports= router
