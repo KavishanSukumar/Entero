6724d6128671d128fdbc3a246330e2c0ea1c91e3
KavishanSukumar <kavishansukumar@gmail.com>
2022-10-26 04:14:36 +0530

    final

--- client/src/components/appointment/AppointmentCustomer.jsx
+++ client/src/components/appointment/AppointmentCustomer.jsx
@@ -134,11 +134,6 @@ function AppointmentCustomer() {
                       <DateRangeIcon /> Date Range
                     </button>
                   </div>
-                  <div className="flex  mx-3 my-3 justify-start lg:justify-end">
-                    <button className="py-2 px-4 w-auto bg-cyan-500 text-white font-semibold rounded-lg shadow-md hover:bg-cyan-400 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75">
-                      <AccessTimeIcon /> Time Range
-                    </button>
-                  </div>
                 </div>
               </div>
               <div className="overflow-auto justify-center w-full h-screen">
@@ -260,11 +255,6 @@ function AppointmentCustomer() {
                       <DateRangeIcon /> Date Range
                     </button>
                   </div>
-                  <div className="flex mx-3 my-3 justify-start lg:justify-end">
-                    <button className="py-2 px-4 w-auto bg-cyan-500 text-white font-semibold rounded-lg shadow-md hover:bg-cyan-400 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75">
-                      <AccessTimeIcon /> Time Range
-                    </button>
-                  </div>
                 </div>
               </div>
               <div className="overflow-auto justify-center w-full h-screen">
@@ -378,11 +368,6 @@ function AppointmentCustomer() {
                       <AccessTimeIcon /> Time Range
                     </button>
                   </div>
-                  <div className="flex basis-3/12 mx-3 my-3 justify-start lg:justify-end">
-                    <button className="py-2 px-4 bg-cyan-500 text-white font-semibold rounded-lg shadow-md hover:bg-cyan-400 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75">
-                      <DateRangeIcon /> Date Range
-                    </button>
-                  </div>
                 </div>
               </div>
               <div className="overflow-auto justify-center w-full h-screen">
@@ -485,11 +470,6 @@ function AppointmentCustomer() {
                   <div className="flex basis-3/12 mx-3 my-3 justify-start lg:justify-end">
                     <button className="py-2 px-4 w-auto bg-cyan-500 text-white font-semibold rounded-lg shadow-md hover:bg-cyan-400 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75">
                       <AccessTimeIcon /> Time Range
-                    </button>
-                  </div>
-                  <div className="flex basis-3/12 mx-3 my-3 justify-start lg:justify-end">
-                    <button className="py-2 px-4 bg-cyan-500 text-white font-semibold rounded-lg shadow-md hover:bg-cyan-400 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75">
-                      <DateRangeIcon /> Date Range
                     </button>
                   </div>
                 </div>
